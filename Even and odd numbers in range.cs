7ba832e3d6b7959900ea72d1cf0fd419af52a160
/Ariya Pedaret <124582449+bravearya@users.noreply.github.com>
2023-02-15 17:14:27 +0330

    Add files via upload

---
+++ Even and odd numbers in range.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication83
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("enter range");
+                int a;
+                int x = 10;
+                string type;
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter (zoje) or (fard)");
+                type = Console.ReadLine();
+                double c = Math.Pow(x, a);
+                for (int range = 0; range < c; range++)
+                {
+                    if (type == "fard")
+                    {
+                        if (range % 2 != 0)
+                        {
+                            string fard = ("fard : " + (range));
+                            Console.WriteLine(fard);
+                            Console.WriteLine("");
+                        }
+                    }
+                    else if (type == "zoje")
+                    {
+                        if (range % 2 == 0)
+                        {
+                            string zoje = ("zoje : " + (range));
+                            Console.WriteLine(zoje);
+                            Console.WriteLine("");
+                        }
+                    }
+
+                } Console.ReadKey();
+            }
+
+
+
+
+
+        }
+    }
+}
